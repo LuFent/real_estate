5dcf293f8195acf1c92aac1bf4a621c170ab52c2
LuFent <vladmarchenkosteam@gmail.com>
2022-02-15 15:37:13 +0300

    dd db model for likes.

--- property/admin.py
+++ property/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Flat
+from .models import Flat, Complaint
 
 
 class FlatAdmin(admin.ModelAdmin):
@@ -8,6 +8,12 @@ class FlatAdmin(admin.ModelAdmin):
     readonly_fields = ['created_at']
     list_display = ['address', 'price', 'new_building', 'construction_year', 'town']
     list_editable = ['new_building']
+    list_filter = ['new_building']
+
+
+class СomplaintAdmin(admin.ModelAdmin):
+    raw_id_fields = ['flat']
 
 
 admin.site.register(Flat, FlatAdmin)
+admin.site.register(Complaint, СomplaintAdmin)
