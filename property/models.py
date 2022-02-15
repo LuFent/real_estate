119fb81f18f1f616e0a4ec3009ce7c5d14516a87
LuFent <vladmarchenkosteam@gmail.com>
2022-02-15 17:09:00 +0300

    given flats to owners

--- property/models.py
+++ property/models.py
@@ -69,11 +69,11 @@ class Complaint(models.Model):
 
 
 class Owner(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
-    owner_pure_phone = PhoneNumberField('Нормализованный номер владельца', blank=True)
+    owner = models.CharField('ФИО владельца', max_length=200, db_index=True)
+    owners_phonenumber = models.CharField('Номер владельца', max_length=20, db_index=True)
+    owner_pure_phone = PhoneNumberField('Нормализованный номер владельца', blank=True, db_index=True)
 
-    flats_owned = models.ManyToManyField(Flat, related_name="owners", verbose_name='Квартиры в собственности')
+    flats_owned = models.ManyToManyField(Flat, related_name="owners", verbose_name='Квартиры в собственности', db_index=True)
 
     def __str__(self):
         return self.owner
